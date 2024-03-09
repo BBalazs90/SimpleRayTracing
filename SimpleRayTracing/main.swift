618827c95d2715213e4bcc0db5256e9a93869a44
Balazs Balogh <balazs.bb.balogh@gmail.com>
2024-03-09 21:25:33 +0100

    ppm file writing initialized

--- SimpleRayTracing/main.swift
+++ SimpleRayTracing/main.swift
@@ -7,5 +7,45 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Vec3f{
+    let X: Float
+    let Y: Float
+    let Z: Float
+}
+
+func render() -> Void{
+    let file = "testfile.ppm"
+    let width = 1024
+    let height = 768
+    
+    var data = Data()
+    
+    data.append(contentsOf: "P6\n".utf8)
+    data.append(contentsOf: "\(width) \(height)\n255\n".utf8)
+    
+    for i in 0..<width*height {
+        for j in 0..<3 {
+            if j == 1 {
+                data.append(contentsOf: [255])
+            }
+            else {
+                data.append(contentsOf: [0])
+            }
+        }
+    }
+    
+    if let dir = FileManager.default.urls(for: .desktopDirectory, in: .userDomainMask).first {
+        let fileUrl = dir.appending(path: file)
+        
+        do {
+            try data.write(to: fileUrl)
+        }
+        catch {
+            // Swallow just now
+        }
+    }
+}
+
+
+render()
 
