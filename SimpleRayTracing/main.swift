8c750b765727b206c56cf8da76283ad06aeea9bc
Balazs Balogh <balazs.bb.balogh@gmail.com>
2024-03-11 20:54:38 +0100

    First sphere render works.

--- SimpleRayTracing/main.swift
+++ SimpleRayTracing/main.swift
@@ -7,10 +7,17 @@
 
 import Foundation
 
+let fieldOfViewAngle:Float = 1.6
+
 struct Vec3f{
     let x: Float
     let y: Float
     let z: Float
+    
+    func normalize() -> Vec3f {
+        let length = sqrtf(x*x + y*y + z*z)
+        return Vec3f(x: x/length, y: y/length, z: z/length)
+    }
     
     static func + (lhs: Vec3f, rhs: Vec3f) -> Vec3f {
         return Vec3f(x: lhs.x + rhs.x, y: lhs.y + rhs.y, z: lhs.z + rhs.z)
@@ -41,15 +48,15 @@ struct Sphere {
     let center: Vec3f
     let radius: Float
     
-    func rayIntersects(startedFrom rayOrigin: Vec3f, towards rayDirection: Vec3f) -> (Bool, Float){
+    func rayIntersects(startedFrom rayOrigin: Vec3f, towards rayDirection: Vec3f) -> (doesIntersect: Bool, intersectionDistance: Float){
         let raySourceToSphereCenter = center - rayOrigin
         let directionProjection = rayDirection * raySourceToSphereCenter
-        let projectionDistanceSquared = raySourceToSphereCenter * raySourceToSphereCenter
-        - directionProjection * directionProjection
+        let projectionDistanceSquared = (raySourceToSphereCenter * raySourceToSphereCenter)
+        - (directionProjection * directionProjection)
         
         if projectionDistanceSquared > radius*radius { return (false, 0.0)}
         
-        let inSphereIntersectionDistance = sqrt(radius*radius - projectionDistanceSquared)
+        let inSphereIntersectionDistance = sqrtf(radius*radius - projectionDistanceSquared)
         let intersectionDistance1 = directionProjection - inSphereIntersectionDistance
         let intersectionDistance2 = directionProjection + inSphereIntersectionDistance
         
@@ -63,6 +70,14 @@ struct Sphere {
     }
 }
 
+func castRay(startedFrom rayOrigin: Vec3f, towards rayDirection: Vec3f, on sphere: Sphere) -> Vec3f{
+    if !sphere.rayIntersects(startedFrom: rayOrigin, towards: rayDirection).doesIntersect{
+        return Vec3f(x: 0.2, y: 0.7, z: 0.8)
+    }
+    
+    return Vec3f(x: 0.4, y: 0.4, z: 0.3)
+}
+
 func render() -> Void{
     let file = "testfile.ppm"
     let width = 1024
@@ -71,7 +86,10 @@ func render() -> Void{
     var buffer = Array<Vec3f>(repeating: Vec3f(x: 0.0, y: 0.0, z: 0.0), count: width*height)
     for j in 0..<height{
         for i in 0..<width {
-            buffer[i + j*width] = Vec3f(x: Float(j)/Float(height), y: Float(i)/Float(width), z: 0.0)
+            let x =  (2.0*(Float(i)+0.5) / Float(width) -  1) * tanf(fieldOfViewAngle/2.0)*Float(width)/Float(height)
+            let y = -(2.0*(Float(j)+0.5) / Float(height) - 1) * tanf(fieldOfViewAngle/2.0)
+            let dir = Vec3f(x: x, y: y, z: -1).normalize()
+            buffer[i + j*width] = castRay(startedFrom: Vec3f(x: 0, y: 0, z: 0), towards: dir, on: Sphere(center: Vec3f(x: -3, y: 0, z: -16), radius: 2))
         }
     }
     
